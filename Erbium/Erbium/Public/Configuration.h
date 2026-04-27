f846cda610289d2f5b7b8a711035fb9eea50c3be
ABDALLHRGX-dev <abdallhrgxdevloper@gmail.com>
2026-04-27 16:10:53 +0300

    Update Configuration.h

--- Erbium/Erbium/Public/Configuration.h
+++ Erbium/Erbium/Public/Configuration.h
@@ -4,17 +4,17 @@ struct FConfiguration
 {
     static inline auto Playlist = L"/Game/Athena/Playlists/Playlist_DefaultSolo.Playlist_DefaultSolo";
     static inline auto MaxTickRate = 30;
-    static inline auto bLateGame = true;
-    static inline auto LateGameZone = 3; // starting zone
-    static inline auto bLateGameLongZone = false; // zone doesnt close for a long time
+    static inline auto bLateGame = false;
+    static inline auto LateGameZone = 1; // starting zone
+    static inline auto bLateGameLongZone = true; // zone doesnt close for a long time
     static inline auto bEnableCheats = true;
     static inline auto SiphonAmount = 50; // set to 0 to disable
-    static inline auto bInfiniteMats = false;
-    static inline auto bInfiniteAmmo = false;
-    static inline auto bForceRespawns = false; // build your client with this too!
-    static inline auto bJoinInProgress = false;
-    static inline auto bAutoRestart = false;
-    static inline auto bKeepInventory = false;
+    static inline auto bInfiniteMats = true;
+    static inline auto bInfiniteAmmo = true;
+    static inline auto bForceRespawns = true; // build your client with this too!
+    static inline auto bJoinInProgress = true;
+    static inline auto bAutoRestart = true;
+    static inline auto bKeepInventory = true;
     static inline auto Port = 7777;
     static inline auto bEnableIris = true;
     static inline constexpr auto bGUI = true;
